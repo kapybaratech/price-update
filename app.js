74fcc3cbc58e4b6ff79a192ba4b0485649bf96a5
Ng Jun Wei <ng.95.jun.wei@gmail.com>
2022-01-31 16:19:53 +0800

    Fixed dependency error

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ const errorMiddleware = (error, req, res, next) => {
     const status = error.status || 500;
     const message = error.message || "Something went wrong";
 
-    logger.error(
-      `[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`
-    );
     res.status(status).json({ message });
   } catch (error) {
     next(error);
